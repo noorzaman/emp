2e3671dde34310198c4b7352da123cffb19e4240
Joanne Huang <jhuang2@atlassian.com>
2018-03-06 11:40:01 +1100

    Add EmpTest component that does search, make EmpTest default instead of HelloWorld

--- emp-project/src/router/index.js
+++ emp-project/src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import EmpTest from '@/components/EmpTest'
 
 Vue.use(Router)
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld
+//     }
+//   ]
+// })
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'EmpTest',
+      component: EmpTest
     }
   ]
 })
