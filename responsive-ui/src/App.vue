345caec0023233374df4c1073fd5a5ddd69a7b1a
H Riggs <hkriggs128@gmail.com>
2018-03-08 16:43:18 -0500

    Responsive UI - browse page rough version

---
+++ responsive-ui/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <div v-if="pageWidth > 846">
+        <DesktopNav></DesktopNav>
+        <div class="space"></div>
+    </div>
+    <div v-else>
+        <MobileHeader></MobileHeader>
+    </div>
+    <router-view/>
+    <div v-if="pageWidth <= 846">
+        <div class="clearFix"></div>
+        <div class="footerSpace"></div>
+        <MobileNav></MobileNav>
+    </div>
+  </div>
+</template>
+
+<script>
+import DesktopNav from './components/DesktopNav'
+import MobileNav from './components/MobileNav'
+import MobileHeader from './components/MobileHeader'
+export default {
+  name: 'App',
+  components: {
+    'DesktopNav': DesktopNav,
+    'MobileNav': MobileNav,
+    'MobileHeader': MobileHeader
+  },
+  data () {
+    return {
+      pageWidth: document.documentElement.clientWidth
+    }
+  },
+  // bind event handlers to the `handleResize` method (defined below)
+  mounted: function () {
+    window.addEventListener('resize', this.handleResize)
+  },
+  beforeDestroy: function () {
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    // whenever the document is resized, re-set the 'pageWidth' variable
+    handleResize (event) {
+      this.pageWidth = document.documentElement.clientWidth
+    }
+  }
+}
+</script>
+
+<style>
+ @import 'https://getbootstrap.com/docs/3.3/dist/css/bootstrap.min.css';
+ @import '../static/css/styles.css';
+</style>
---
+++ responsive-ui/src/components/Booked.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main">
+    <h1>MY BOOKED SPACES</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
