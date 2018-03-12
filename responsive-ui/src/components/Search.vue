f7d9af54658572950a72eb98e5ad9ea6e7b330b0
Joanne Huang <jhuang2@atlassian.com>
2018-03-13 10:56:04 +1100

    wip

--- responsive-ui/src/components/Search.vue
+++ responsive-ui/src/components/Search.vue
@@ -34,10 +34,123 @@
         </div>
         <input type="submit" value="Search Spaces"/>
     </form>
+    <table id="resultsTable" border="1" cellpadding="10px">
+      <tr>
+        <th>Space Name</th>
+        <th>Space Email</th>
+        <th>Space Description</th>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+export default {
+  name: 'Search',
+  components: {
+  },
+  data () {
+    return {
+      pageTitle: 'Search Meeting Spaces',
+      pageWidth: document.documentElement.clientWidth,
+      empUrl: '',
+      searchResult: ''
+    }
+  },
+  // bind event handlers to the `handleResize` method (defined below)
+  mounted: function () {
+    window.addEventListener('resize', this.handleResize)
+    this.createElasticSearchUrl()
+  },
+  beforeDestroy: function () {
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    // whenever the document is resized, re-set the 'pageWidth' variable
+    handleResize (event) {
+      this.pageWidth = document.documentElement.clientWidth
+    },
+    createElasticSearchUrl () {
+      var empHost = 'https://search-emp-cixk22lczi5yrt4zd2dhswnltm.us-east-1.es.amazonaws.com'
+      var empIndex = 'emp'
+      var empType = 'rooms'
+      this.empUrl = empHost + '/' + empIndex + '/' + empType
+    },
+    searchByTheme (theme) {
+      var jsonStr = '{"query": {"simple_query_string" : {"fields" : ["meeting_place.theme"], "query" : "' + theme + '"}}}'
+      this.sendSearchAndDisplayResultrchAndDisplayResult(jsonStr)
+    },
+    searchByEmail (email) {
+      var jsonStr = '{"query": {"ids" : {"values" : ["' + email + '"]}}}'
+      this.sendSearchAndDisplayResultrchAndDisplayResult(jsonStr)
+    },
+    searchByAttributes (attributes) {
+      var jsonStr = '{"query": {"simple_query_string" : {"fields" : ["tags"], "query" : "' + attributes + '"}}}'
+      this.sendSearchAndDisplayResultrchAndDisplayResult(jsonStr)
+    },
+    searchByField (fields, keywords) {
+      var jsonStr = ''
+
+      if ((fields === null || fields === undefined || fields.trim() === '') &&
+        (keywords === null || keywords === undefined || keywords.trim() === '')) {
+        jsonStr = '{"query":{"match_all":{}},' + '"sort":[{"meeting_place.update_date":{"order":"desc"}}]}'
+      } else if ((keywords !== null && keywords !== undefined && keywords.trim() !== '') &&
+        (fields !== null && fields !== undefined && fields.trim() !== '')) {
+        jsonStr = '{"query": {"simple_query_string" : {"fields" : ["' + fields + '"], "query" : "' + keywords + '"}}}'
+      } else if (keywords === null || keywords === undefined || keywords.trim() === '') {
+        jsonStr = '{"query": {"simple_query_string" : {"fields" : ["' + fields + '"], "query" : "' + '*' + '"}}}'
+      } else if (fields === null || fields === undefined || fields.trim() === '') {
+        jsonStr = '{"query": {"multi_match" : ' + '{"fields" : ["' + '*' + '"],' + '"query" : "' + keywords + '"}}}'
+      }
+      this.sendSearchAndDisplayResult(jsonStr)
+    },
+    matchAll () {
+      var jsonStr = '{"query":{"match_all":{}},' + '"sort":[{"meeting_place.update_date":{"order":"desc"}}]}'
+      this.sendSearchAndDisplayResult(jsonStr)
+    },
+    sendSearchAndDisplayResult (jsonStr) {
+      var searchUrl = this.empUrl + '/_search?size=50&from=0'
+      this.$http.post(searchUrl, jsonStr, {
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8'
+        }
+      }).then(result => {
+        this.searchResult = result.body.hits.hits
+
+        var top5ResultsArray = []
+
+        for (var n = 0; n < (this.searchResult.length > 5 ? 5 : this.searchResult.length); n++) {
+          var matchedEntry = this.searchResult[n]._source.meeting_place
+          top5ResultsArray[n] = [matchedEntry.name, matchedEntry.email, matchedEntry.description]
+        }
+
+        var table = document.getElementById('resultsTable')
+
+        // clear table except heading
+        var rowCount = table.rows.length
+        for (var r = rowCount - 1; r > 0; r--) {
+          table.deleteRow(r)
+        }
+
+        // for (var i = 0; i < 5; i++) {
+        for (var i = 0; i < top5ResultsArray.length; i++) {
+          // create a new row
+          var newRow = table.insertRow(table.length)
+          // for (var j = 0; j < 3; j++) {
+          for (var j = 0; j < top5ResultsArray[i].length; j++) {
+            // create a new cell
+            var cell = newRow.insertCell(j)
+
+            // add value to the cell
+            cell.innerHTML = top5ResultsArray[i][j]
+          }
+        }
+      }, error => {
+        console.error(error)
+      })
+    }
+  }
+}
 </script>
 
 <style scoped>
