beceaa35ef8b76d6a45c4602016af5f09d8ccafa
sdmogensen <sdmogensen@gmail.com>
2018-03-13 18:14:55 -0400

    fixed theme search

--- responsive-ui/src/components/BrowseAll.vue
+++ responsive-ui/src/components/BrowseAll.vue
@@ -10,18 +10,10 @@
     </div>
     <div v-for="theme in themes" v-bind:key="theme" class="browseThemes">
         <a :href="'/browse/' + theme.image">
-        <!-- v-on:click="searchByTheme(theme.image)"> -->
             <img :src="'/static/images/' + theme.image + '.png'"  :alt="theme.caption">
             <p>{{theme.caption}}</p>
         </a>
     </div>
-    <!-- <table id="resultsTable" border="1" cellpadding="10px">
-      <tr>
-        <th>Space Name</th>
-        <th>Space Email</th>
-        <th>Space Description</th>
-      </tr>
-    </table> -->
   </div>
 </template>
 
@@ -49,15 +41,12 @@ export default {
         {image: 'zen', caption: 'Zen'}
       ],
       pageTitle: 'Explore Meeting Spaces',
-      pageWidth: document.documentElement.clientWidth,
-      empUrl: '',
-      searchResult: ''
+      pageWidth: document.documentElement.clientWidth
     }
   },
   // bind event handlers to the `handleResize` method (defined below)
   mounted: function () {
     window.addEventListener('resize', this.handleResize)
-    this.createElasticSearchUrl()
   },
   beforeDestroy: function () {
     window.removeEventListener('resize', this.handleResize)
@@ -66,57 +55,6 @@ export default {
     // whenever the document is resized, re-set the 'pageWidth' variable
     handleResize (event) {
       this.pageWidth = document.documentElement.clientWidth
-    },
-    createElasticSearchUrl () {
-      var empHost = 'https://search-emp-cixk22lczi5yrt4zd2dhswnltm.us-east-1.es.amazonaws.com'
-      var empIndex = 'emp'
-      var empType = 'rooms'
-      this.empUrl = empHost + '/' + empIndex + '/' + empType
-    },
-    searchByTheme (clickedTheme) {
-      var jsonStr = '{"query": {"simple_query_string" : {"fields" : ["meeting_place.theme"], "query" : "' + clickedTheme + '"}}}'
-      this.sendSearchAndDisplayResult(jsonStr)
-    },
-    sendSearchAndDisplayResult (jsonStr) {
-      var searchUrl = this.empUrl + '/_search?size=50&from=0'
-      this.$http.post(searchUrl, jsonStr, {
-        headers: {
-          'Content-Type': 'application/json;charset=UTF-8'
-        }
-      }).then(result => {
-        this.searchResult = result.body.hits.hits
-
-        var top5ResultsArray = []
-
-        for (var n = 0; n < (this.searchResult.length > 5 ? 5 : this.searchResult.length); n++) {
-          var matchedEntry = this.searchResult[n]._source.meeting_place
-          top5ResultsArray[n] = [matchedEntry.name, matchedEntry.email, matchedEntry.description]
-        }
-
-        var table = document.getElementById('resultsTable')
-
-        // clear table except heading
-        var rowCount = table.rows.length
-        for (var r = rowCount - 1; r > 0; r--) {
-          table.deleteRow(r)
-        }
-
-        // for (var i = 0; i < 5; i++) {
-        for (var i = 0; i < top5ResultsArray.length; i++) {
-          // create a new row
-          var newRow = table.insertRow(table.length)
-          // for (var j = 0; j < 3; j++) {
-          for (var j = 0; j < top5ResultsArray[i].length; j++) {
-            // create a new cell
-            var cell = newRow.insertCell(j)
-
-            // add value to the cell
-            cell.innerHTML = top5ResultsArray[i][j]
-          }
-        }
-      }, error => {
-        console.error(error)
-      })
     }
   }
 }
