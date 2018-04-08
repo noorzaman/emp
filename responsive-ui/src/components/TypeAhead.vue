f2f87d18adb013db255b91c3d68d5c65d5158a6d
Nurul Zaman <nurul.zaman@gmail.com>
2018-04-07 19:53:19 -0700

    moved call to methods instead of compute

--- responsive-ui/src/components/TypeAhead.vue
+++ responsive-ui/src/components/TypeAhead.vue
@@ -4,7 +4,7 @@
     <p>Ex: food, WiFi, projector, etc.</p>
     <input id="attributes" class="form-control" type="text" placeholder="Type to search..." autocomplete="off" @keyup.enter="addAttribute">
     <!-- typeahead component is defined at: https://uiv.wxsm.space/typeahead/ -->
-    <typeahead v-model="newAttribute" target="#attributes" :data="attributes" item-key="name" :open-on-empty="true"/>
+    <typeahead v-model="newAttribute" target="#attributes" :data="attributes()" item-key="name" :open-on-empty="true"/>
     <p class="text-danger">{{newAttributeError}}</p>
     <button id="addAttrButton" class="btn btn-primary" @click="addAttribute">Add</button>
 
@@ -88,13 +88,9 @@ export default {
       }
       //  Clear the typeahead input box.
       this.newAttribute = null
-    }
-  },
-
-  computed: {
+    },
     attributes: function () {
       var arrUniq = []
-
       var search = {
         'query': {
           'term': {
@@ -104,7 +100,7 @@ export default {
       }
       var jsonStr = JSON.stringify(search)
       var searchUrl = 'https://search-emp-cixk22lczi5yrt4zd2dhswnltm.us-east-1.es.amazonaws.com/emp/rooms/_search?pretty&_source=space.attributes'
-      this.$http.post(searchUrl, jsonStr, {
+      this.$http.get(searchUrl, jsonStr, {
         headers: {
           'Content-Type': 'application/json;charset=UTF-8'
         }
@@ -132,7 +128,6 @@ export default {
       })
       return arrUniq
     }
-
   }
 }
 </script>
