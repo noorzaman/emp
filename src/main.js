eff9011af03f8d3261edec91809ca4b3cb454645
Nurul Zaman <nurul.zaman@gmail.com>
2018-04-25 14:22:37 -0700

    externalizing endpoints for urls

--- src/main.js
+++ src/main.js
@@ -15,11 +15,19 @@ Vue.http.options.emulateJSON = true
 const http = Vue.http
 export default http
 Vue.config.productionTip = false
-Vue.prototype.$searchUrl = 'https://search-emp-cixk22lczi5yrt4zd2dhswnltm.us-east-1.es.amazonaws.com/emp/rooms'
-Vue.prototype.$commonTagsUrl = 'https://search-emp-cixk22lczi5yrt4zd2dhswnltm.us-east-1.es.amazonaws.com/_all'
-Vue.prototype.$editUrl = 'https://3p8vyivi98.execute-api.us-east-1.amazonaws.com/dev/place'
-Vue.prototype.$availabilityUrl = 'http://development.6awinxwfj9.us-east-1.elasticbeanstalk.com/availability/'
-Vue.prototype.$googleCalendarUrl = 'http://localhost:5000/'
+
+// Vue.prototype.$searchUrl = 'https://search-emp-cixk22lczi5yrt4zd2dhswnltm.us-east-1.es.amazonaws.com/emp/rooms'
+// Vue.prototype.$commonTagsUrl = 'https://search-emp-cixk22lczi5yrt4zd2dhswnltm.us-east-1.es.amazonaws.com/_all'
+// Vue.prototype.$editUrl = 'https://3p8vyivi98.execute-api.us-east-1.amazonaws.com/dev/place'
+// Vue.prototype.$availabilityUrl = 'http://development.6awinxwfj9.us-east-1.elasticbeanstalk.com/availability/'
+// Vue.prototype.$googleCalendarUrl = 'http://localhost:5000/'
+
+Vue.prototype.$searchUrl = process.env.SEARCH_ADDR + '/emp/rooms'
+Vue.prototype.$commonTagsUrl = process.env.COMMON_TAGS_ADDR + '/_all'
+Vue.prototype.$editUrl = process.env.EDIT_ADDR + '/dev/place'
+Vue.prototype.$availabilityUrl = process.env.AVAILABILITY_ADDR + '/availability/'
+Vue.prototype.$googleCalendarUrl = process.env.GOOGLE_CALENDAR_ADDR + '/'
+
 Vue.prototype.$defaultHeaders = {'Content-Type': 'application/json;charset=UTF-8'}
 Vue.prototype.$emailRegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 Vue.prototype.$possibleThemes = ['casual', 'celebratory', 'cozy', 'fancy', 'fun', 'lively', 'modern', 'professional', 'quiet', 'rustic', 'studious', 'zen']
