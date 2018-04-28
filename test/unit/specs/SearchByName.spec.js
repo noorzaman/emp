af49803bc2b43529cde7bc0f60b8890641db79ae
Ashish Surana <asurana@g.harvard.edu>
2018-04-28 15:26:25 -0400

    EMP-9 Added a test to verify the Search by name panel opens up when changeSearchToByName() is called

---
+++ test/unit/specs/SearchByName.spec.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Search from '@/components/Search'
+
+/** This test spec tests search space by name feature.
+  */
+
+describe('Search.vue', () => {
+  const Constructor = Vue.extend(Search)
+  const SearchComponent = new Constructor().$mount()
+
+  it('shows Search by name page when changeSearchToByName() is called', () => {
+  	SearchComponent.changeSearchToByName()
+    expect(SearchComponent.$el.textContent).to.contain('Space Name')
+    expect(SearchComponent.showFullSearch).to.equal(false)
+  })
+})
