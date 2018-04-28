42f95ca7c688e24a314670e6537e525ca74e9f12
Nurul Zaman <nurul.zaman@gmail.com>
2018-04-27 20:28:23 -0700

    slimming down mobile nav

--- src/components/MobileNav.vue
+++ src/components/MobileNav.vue
@@ -7,7 +7,7 @@
           <div v-else>
             <img class="mobile-nav-icon" src="/static/images/home.png" alt="Browse">
           </div>
-          <p>Browse</p>
+          <p class="mobile-nav-text">Browse</p>
         </router-link>
         <router-link to="/search-spaces" class="item" id="searchNavLink" v-bind:class="{ 'mobileNavActive': pageLocation === 'search' }">
           <div v-if="pageLocation === 'search'">
@@ -16,7 +16,7 @@
           <div v-else>
             <img class="mobile-nav-icon" src="/static/images/search.png" alt="Search">
           </div>
-          <p>Search</p>
+          <p class="mobile-nav-text">Search</p>
         </router-link>
         <router-link to="/booked-spaces" class="item" v-bind:class="{ 'mobileNavActive': pageLocation === 'booked' }">
           <div v-if="pageLocation === 'booked'">
@@ -25,7 +25,7 @@
           <div v-else>
             <img class="mobile-nav-icon" src="/static/images/booked.png" alt="Booked Spaces">
           </div>
-          <p>Booked</p>
+          <p class="mobile-nav-text">Booked</p>
         </router-link>
         <router-link to="/add-space" class="item" v-bind:class="{ 'mobileNavActive': pageLocation === 'addspace' }">
           <div v-if="pageLocation === 'addspace'">
@@ -34,7 +34,7 @@
           <div v-else>
             <img class="mobile-nav-icon" src="/static/images/add.png" alt="Add Space">
           </div>
-          <p>Add</p>
+          <p class="mobile-nav-text">Add</p>
         </router-link>
     </div>
 </template>
